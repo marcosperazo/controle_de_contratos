da5d237f2030fa30c9d2d539884af4e28f290d8e
marcosperazo <marcosperazo@icloud.com>
2021-11-06 00:18:08 -0300

    criação do ImovelRepository e ContratoRepository, e ajuste no navbar

--- src/main/java/com/perazo/controle_de_contratos/repository/ContratoRepository.java
+++ src/main/java/com/perazo/controle_de_contratos/repository/ContratoRepository.java
@@ -3,9 +3,9 @@ package com.perazo.controle_de_contratos.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.perazo.controle_de_contratos.model.Cliente;
+import com.perazo.controle_de_contratos.model.Contrato;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-		
+public interface ContratoRepository extends JpaRepository<Contrato, Long>{
+
 }
--- src/main/java/com/perazo/controle_de_contratos/repository/ImovelRepository.java
+++ src/main/java/com/perazo/controle_de_contratos/repository/ImovelRepository.java
@@ -3,9 +3,9 @@ package com.perazo.controle_de_contratos.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.perazo.controle_de_contratos.model.Cliente;
+import com.perazo.controle_de_contratos.model.Imovel;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-		
+public interface ImovelRepository extends JpaRepository<Imovel, Long>{
+
 }
