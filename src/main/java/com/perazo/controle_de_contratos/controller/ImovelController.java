1144a3077805798c06c7de1884f012478b38f9e4
marcosperazo <marcosperazo@icloud.com>
2021-11-10 17:31:47 -0300

    alteração do ImovelController e criação da páginas detalhes-imovel, form-editar e formulario, todas relacionadas ao Imóvel

--- src/main/java/com/perazo/controle_de_contratos/controller/ImovelController.java
+++ src/main/java/com/perazo/controle_de_contratos/controller/ImovelController.java
@@ -2,12 +2,18 @@ package com.perazo.controle_de_contratos.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.perazo.controle_de_contratos.dto.RequisicaoImovel;
 import com.perazo.controle_de_contratos.model.Imovel;
 import com.perazo.controle_de_contratos.repository.ImovelRepository;
 
@@ -19,10 +25,64 @@ public class ImovelController {
 	private ImovelRepository imovelRepository;
 
 	@GetMapping("")
-	public String Clientes(Model model) {
+	public String Imoveis(Model model) {
 		
 		List<Imovel> imoveis = imovelRepository.findAll();
 		model.addAttribute("imoveis", imoveis);
 		return "imovel/imoveis";
 	}
+
+	@GetMapping("/formulario")
+	public String formulario(RequisicaoImovel requisicao) {
+		
+		return "imovel/formulario";
+	}
+	
+	@PostMapping("/novo")
+	public String novo(@Valid RequisicaoImovel requisicao, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "imovel/formulario";
+		}
+		
+		Imovel imovel = requisicao.toImovel();
+		imovelRepository.save(imovel);
+		
+		return "redirect:/imoveis";
+	}
+	
+	@GetMapping("/{id}")
+	public String imoveldetalhes(@PathVariable("id") Long id, Model model) {
+		Imovel imovel = imovelRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("id " + id + " é inválido"));
+		model.addAttribute("imovel", imovel);
+		return "imovel/detalhes-imovel";
+	}
+		
+	@GetMapping("/delete/{id}")
+	public String deleteImovel(@PathVariable("id") Long id, Model model) {
+		Imovel imovel = imovelRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("id " + id + " é inválido"));
+		imovelRepository.delete(imovel);
+		return "redirect:/imoveis";
+	}
+	
+	@GetMapping("/atualizarForm/{id}")
+	public String mostrarFormEditar(@PathVariable("id") Long id, Model model) {
+		Imovel imovel = imovelRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("id " + id + " é inválido"));
+		model.addAttribute("imovel", imovel);
+		return "imovel/form-editar";
+	}
+	
+	
+	@PostMapping("/update/{id}")
+	public String update(@PathVariable("id") long id, @Valid Imovel imovel, BindingResult result, Model model) {
+	    if (result.hasErrors()) {
+	        return "imovel/form-editar";
+	    }
+	    
+	    imovelRepository.save(imovel);
+	    return "redirect:/imoveis";
+	}
+	
+		
 }
+
