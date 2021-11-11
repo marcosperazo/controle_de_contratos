203815b098390b10d5f5096c43fe8d060d5042d5
marcosperazo <marcosperazo@icloud.com>
2021-11-11 20:58:50 -0300

    criação do página de detalhes do contrato e alteração do ContratoController

--- src/main/java/com/perazo/controle_de_contratos/controller/ContratoController.java
+++ src/main/java/com/perazo/controle_de_contratos/controller/ContratoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.perazo.controle_de_contratos.model.Contrato;
@@ -25,5 +26,22 @@ public class ContratoController {
 		model.addAttribute("contratos", contratos);
 		return "contrato/contratos";
 	}
+
+	@GetMapping("/{id}")
+	public String contratoDetalhes(@PathVariable("id") Long id, Model model) {
+		Contrato contrato = contratoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("id " + id + " é inválido"));
+		model.addAttribute("contrato", contrato);
+		return "contrato/detalhes-contrato";
+	}
+		
+	@GetMapping("/delete/{id}")
+	public String deleteContrato(@PathVariable("id") Long id, Model model) {
+		Contrato contrato = contratoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("id " + id + " é inválido"));
+		contratoRepository.delete(contrato);
+		return "redirect:/contratos";
+	}
+	
+	
+	
 	
 }
